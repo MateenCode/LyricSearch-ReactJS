94876dcaab9e53117792873e262f08e75c605611
Mateen kazia <mateenkazia@Mateens-MacBook-Pro.local>
2018-11-19 11:09:35 -0800

    used axios and context to display top ten tracks

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default () => {
+  return (
+    <div>
+        <img src='https://media.giphy.com/media/3o7abKhOpu0NwenH3O/giphy.gif'
+            alt="Load..."
+            style={{ width: '200px', margin: ' 40px auto', display: 'block' }}/>
+    </div>
+  )
+}
