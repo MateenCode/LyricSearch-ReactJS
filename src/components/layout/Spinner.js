0fe759fc5e057e79095227af050280c37509cace
Mateen kazia <mateenkazia@Mateens-MacBook-Pro.local>
2018-11-21 10:17:05 -0800

    lyric page added all values displaying properly

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default () => {
   return (
     <div>
-        <img src='https://media.giphy.com/media/3o7abKhOpu0NwenH3O/giphy.gif'
+        <img src='https://ubisafe.org/images/transparent-gif-spinner-1.gif'
             alt="Load..."
             style={{ width: '200px', margin: ' 40px auto', display: 'block' }}/>
     </div>
